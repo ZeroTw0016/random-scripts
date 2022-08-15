f239d4bf48befd2bcc8dcab8f5c166b82ac42e5c
ZeroTwo <thezerotwo016@gmail.com>
2022-08-15 22:25:21 +0200

    a

--- seamlessCOOP/updater-installer.ps1
+++
@@ -1,5 +0,0 @@
-Requires -RunAsAdministrator
-(wget https://raw.githubusercontent.com/ZeroTw0016/random-scripts/main/seamlessCOOP/updater.ps1).content > updater.ps1
-Install-Module -Name ps2exe -Confirm
-Invoke-ps2exe -inputFile .\updater.ps1 -outputFile .\updater.exe -iconFile .\icon.jpg
-New-Item -ItemType SymbolicLink -Path updater.exe -Target [Environment]::GetFolderPath("Desktop")
