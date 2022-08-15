6a63a34e652971227cf09add751f588062f132ba
ZeroTwo <thezerotwo016@gmail.com>
2022-08-15 23:01:26 +0200

    Update installer.ps1

--- seamlessCOOP/installer.ps1
+++ seamlessCOOP/installer.ps1
@@ -1,8 +1,5 @@
 New-Item -Path "$HOME/ZeroCode/" -ItemType Directory -Force -Name "scripts"
-$file = 
-if(!Test-Path -Path "$HOME/ZeroCode/scripts/updater.ps1") {
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ZeroTw0016/random-scripts/main/seamlessCOOP/updater.ps1" -OutFile "$HOME/ZeroCode/scripts/updater.ps1"
-}
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ZeroTw0016/random-scripts/main/seamlessCOOP/updater.ps1" -OutFile "$HOME/ZeroCode/scripts/updater.ps1"
 Set-Location "$HOME/ZeroCode/scripts/updater.ps1"
 start powershell {.\updater.ps1}
 
