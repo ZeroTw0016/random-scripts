5fb726bc19f4af2aab63650fb9ae077c45815e13
ZeroTwo <thezerotwo016@gmail.com>
2022-08-15 21:15:41 +0200

    Update updater.ps1

--- seamlessCOOP/updater.ps1
+++ seamlessCOOP/updater.ps1
@@ -11,8 +11,37 @@ $ver = [int]$vers
 $onVer = [int]$onVers
 $modVer = $modVers
 
+$eldenFolder = ""
+$steamFolders = @()
+
+
+function replaceLine($textContent,$match,$updateLineContent){
+    $line = $textContent | Select-String $match | Select-Object -ExpandProperty Line
+    return $textContent -replace $line,$updateLineContent
+}
+
 foreach($drive in $drives) {
-    #Write-Host $drive
+    $path = $drive.Root
+    $content = Get-ChildItem $path -Force -Directory -ErrorAction SilentlyContinue
+    foreach($folder in $content) {
+        if($folder.Name -eq "SteamLibrary") {
+            $steamFolders += $folder.FullName
+        }
+        if($folder.Name -eq "Steam") {
+            $steamFolders += $folder.FullName
+        }
+    }
+}
+
+foreach($folder in $steamFolders) {
+    $path = $folder + "\steamapps\common\"
+    $folders = Get-ChildItem $path -Directory -ErrorAction SilentlyContinue
+    foreach($folder in $folders) {
+        if($folder.Name -eq "ELDEN RING") {
+            $name = $folder.FullName + "\Game\"
+            $eldenFolder = $name
+        }
+    }
 }
 if(Test-Path -Path $versionFile) {
     if($ver -lt $onVer) {
@@ -21,11 +50,8 @@ if(Test-Path -Path $versionFile) {
         $script = ((Invoke-WebRequest -Uri $rawScriptUrl)).content | Out-String
         Set-Content ./updater.ps1 -Value $script
         Start-Sleep 1
-        #start powershell {.\updater.ps1}
+        start powershell {.\updater.ps1}
         exit
-    }
-    else {
-        Write-Host "no script update needed"
     }
 }
 else {
@@ -35,12 +61,39 @@ else {
 }
 
 $tmp = "$HOME/ZeroCode/ZeroTMP"
-New-Item -ItemType Directory -Force -Path "$tmp"
+New-Item -ItemType Directory -Force -Path "$tmp" | Out-Null
+New-Item -ItemType File -Force -Path "$modVersionFile" | Out-Null
 $url = 'https://github.com/LukeYui/EldenRingSeamlessCoopRelease/releases/latest'
 
 $site = ((Invoke-WebRequest -Uri $url).Links.href) -like "*/download/*" | Select-Object -first 1 | Out-String
 $site = "https://github.com/$site"
 $fileName = ([uri]$site).Segments[-1]
-Invoke-WebRequest -Uri $site -OutFile "$tmp/$fileName" 
-
-## Add auto folder mgmt
+if($fileName -ne $modVer) {
+    Invoke-WebRequest -Uri $site -OutFile "$tmp/$fileName"
+    Expand-Archive -Path "$tmp/$fileName" -DestinationPath "$tmp/" -Force
+    Remove-Item -Path "$tmp/$fileName"
+    Move-Item -Path "$tmp/launch_elden_ring_seamlesscoop.exe" -Destination $eldenFolder -Force
+    $coopFolder = $eldenFolder + "/SeamlessCoop/"
+    $settingsFile = $coopFolder + "seamlesscoopsettings.ini"
+    $tmpsettingsFile = $tmp + "/SeamlessCoop/seamlesscoopsettings.ini"
+    Move-Item -Path "$tmp/SeamlessCoop/elden_ring_seamless_coop.dll" -Destination $coopFolder -Force
+    $settings = Get-Content -Path $settingsFile -ErrorAction SilentlyContinue
+    foreach($line in $settings) {
+        if(!$line.contains(";") -and !$line.contains("[") -and $line -ne "") {
+            $pos = $line.IndexOf("=")
+            $leftPart = $line.Substring(0, $pos)
+            $rightPart = $line.Substring($pos+1)
+            if($leftPart -like "cooppassword ") {
+                $pwd = "cooppassword =" + $rightPart
+            }
+        }
+    }
+    $tmpSettings = Get-Content -Path $tmpsettingsFile -ErrorAction SilentlyContinue
+    $tmpSettings -replace '^[^;]*\=+ $',$pwd | Set-Content $tmpsettingsFile
+    Move-Item -Path $tmpsettingsFile -Destination $coopFolder -Force
+    Remove-Item $tmp -Force -Recurse
+    Set-Content -Path $modVersionFile -Value $fileName
+}
+else {
+    Write-Host "no update needed Launching the game"
+}
