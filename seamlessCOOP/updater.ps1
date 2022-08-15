a4400ec540e8b101a283c5c6086ec1cf8affdd66
ZeroTw0016 <84594740+ZeroTw0016@users.noreply.github.com>
2022-08-15 17:55:03 +0200

    Update updater.ps1

--- seamlessCOOP/updater.ps1
+++ seamlessCOOP/updater.ps1
@@ -1,18 +1,21 @@
 $versionFile = "$env:APPDATA/ZeroCode/SeamlessCOOP/version.txt"
 $rawScriptUrl = "https://raw.githubusercontent.com/ZeroTw0016/random-scripts/main/seamlessCOOP/updater.ps1"
 $onVerUrl = "https://raw.githubusercontent.com/ZeroTw0016/random-scripts/main/seamlessCOOP/version"
+$onVer = ((Invoke-WebRequest -Uri $onVerUrl)).content | Out-String
 
 if(Test-Path -Path $versionFile) {
     $ver = Get-Content -Path $versionFile
-    $onVer = ((Invoke-WebRequest -Uri $onVerUrl)).content | Out-String
     if(!$ver -eq $onVer) {
         Write-host "Script Updating"
         $script = ((Invoke-WebRequest -Uri $rawScriptUrl)).content | Out-String
         Set-Content ./updater.ps1 -Value $script
+        ./updater.ps1
+        exit
     }
 }
 else {
     New-Item -Path $versionFile -ItemType "File"
+    Set-Content $versionFile -Value $onVer
 }
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $tmp = "$HOME/ZeroTMP"
